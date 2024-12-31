23ad713c5ef7a2a98f994400ea2093697cac60b4
AustenVines <up2124146@myport.ac.uk>
2024-12-31 10:06:26 +0000

    list of saves now visible

--- android/app/src/main/kotlin/com/example/base_application/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/base_application/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.base_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+//package com.example.base_application
+//
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity()
--- lib/displayPages/startUpPage.dart
+++ lib/displayPages/startUpPage.dart
@@ -1,5 +1,9 @@
 import 'package:base_application/displayPages/gamePage.dart';
+import 'package:base_application/services/firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+FirestoreService firestoreService = FirestoreService();
 
 class StartupPage extends StatefulWidget {
   const StartupPage({super.key});
@@ -13,7 +17,8 @@ class StartupPageState extends State<StartupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
-      body: Column(
+      body:
+          Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [const Row(
@@ -26,7 +31,38 @@ class StartupPageState extends State<StartupPage> {
         children: [
           TextButton(onPressed: () {Navigator.push(context,
           MaterialPageRoute(builder: (context) => const GamePage()));},
-          child: const Text("Play"))],),
+          child: const Text("Play")),
+          SizedBox(
+
+            height: 200,
+            width: 200,
+            child: StreamBuilder<QuerySnapshot>(
+                stream: firestoreService.getSavesStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData){
+                    List savesList = snapshot.data!.docs;
+                    return ListView.builder(
+                        itemCount: savesList.length,
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot document = savesList[index];
+                          String docID = document.id;
+
+                          Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                          String saveName = data['history'];
+
+                          return ListTile(
+                            title: Text(saveName),
+                          );
+                        });
+
+                  }
+                  else{
+                    return const Text("no saves");
+                  }
+                }),
+          )
+        ]),
+
         ],
       ),
     );
